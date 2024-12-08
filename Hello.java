da5b80d0e709b2dfe06ed8b45d00d8e75c5dfc79
iiTzShaa <psnsharon@gmail.com>
2024-12-08 18:28:19 +0200

    fix Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Hello {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -8,7 +8,7 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("Hello World ..."+i);
+            System.out.println("Hello World ...."+i);
 
         }
     }
